459aeee55418755c0525c66442338132a4da46bf
crehds <carlos15erh@gmail.com>
2026-04-28 21:24:03 -0500

    Add CSS styles and serve static files in the Node.js server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import { readFile } from "node:fs/promises";
 import { createServer } from "node:http";
+import { extname, join, resolve } from "node:path";
+
+console.log("Probando");
 
 const html = `
     <!doctype html>
@@ -7,6 +10,8 @@ const html = `
       <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="/styles.css" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
           <title>Vanilla Node Web Server</title>
       </head>
       <body>
@@ -21,7 +26,7 @@ const html = `
           <section>
             <h2>Conoce a la mascota</h2>
             <figure>
-              <img src="/node-mascot.svg" alt="Mascota de Node.js" width="120" />
+              <img src="node-mascot.svg" alt="Mascota de Node.js" width="120" />
               <figcaption>Mascota de Node.js</figcaption>
             </figure>
           </section>
@@ -29,6 +34,7 @@ const html = `
     </html>
   `;
 
+const a = 2;
 async function requestListener(req, res) {
   const { url } = req;
 
@@ -51,13 +57,50 @@ async function requestListener(req, res) {
     res.end(JSON.stringify({ time: time }));
   }
 
-  if (url === "/node-mascot.svg") {
+  // if (url === "/node-mascot.svg") {
+  //   try {
+  //     const data = await readFile("node-mascot.svg");
+  //     // Es clave enviar el Content-Type correcto para que el navegador sepa qué hacer con los datos
+  //     res.writeHead(200, { "Content-Type": "image/svg+xml" });
+  //     return res.end(data);
+  //   } catch {
+  //     res.writeHead(404);
+  //     return res.end();
+  //   }
+  // }
+
+  const PUBLIC_DIR = resolve("public");
+  // Asumimos el riesgo de un ataque 'Directory Traversal' por ahora
+  const filePath = join(PUBLIC_DIR, url);
+  const ext = extname(url).toLowerCase();
+
+  if (ext === ".svg") {
     try {
-      const data = await readFile("node-mascot.svg");
-      // Es clave enviar el Content-Type correcto para que el navegador sepa qué hacer con los datos
+      const data = await readFile(filePath);
       res.writeHead(200, { "Content-Type": "image/svg+xml" });
       return res.end(data);
     } catch {
+      // Si el archivo no existe, simplemente respondemos 404
+      res.writeHead(404);
+      return res.end();
+    }
+  } else if (ext === ".css") {
+    try {
+      const data = await readFile(filePath);
+      res.writeHead(200, { "Content-Type": "text/css" });
+      return res.end(data);
+    } catch {
+      // Si el archivo no existe, simplemente respondemos 404
+      res.writeHead(404);
+      return res.end();
+    }
+  } else if (ext === ".ico") {
+    try {
+      const data = await readFile(filePath);
+      res.writeHead(200, { "Content-Type": "image/x-icon" });
+      return res.end(data);
+    } catch {
+      // Si el archivo no existe, simplemente respondemos 404
       res.writeHead(404);
       return res.end();
     }
